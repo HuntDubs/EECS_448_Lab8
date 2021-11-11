ea965c3552aedb6d68b05ce56cc274118edacc63
HuntDubs <Hunterwilson4092@gmail.com>
2021-11-11 15:55:06 -0600

    Update

--- Exercise_2/slideshow.js
+++ Exercise_2/slideshow.js
@@ -1,3 +1,32 @@
-function slideShow(){
+function slide(direction){
+    let path = document.getElementById("imageID");
+    let headshots = [];
+    for (let x =1; x<=5; x++){
+        headshots[x] = "headshot"+x;
+    }
+    let currentPic = 1;
+    path.src = "images/"+headshots[currentPic];
+    if (direction == 0){
+        currentPic = changePic(currentPic, direction);
+        path.src="images/"+headshots[currentPic]
+    } else if (direction == 1){
+        
+    }
+}
+
+function changePic(picNumber, direction){
+    if (direction == 0){
+        if (picNumber == 1){
+            return 5;
+        } else {
+            return picNumber -1;
+        }
+    } else if (direction == 1){
+        if (picNumber == 5){
+            return 1;
+        } else {
+            return picNumber + 1;
+        }
+    }
     
 }
